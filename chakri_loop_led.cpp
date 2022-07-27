5e01e57cf54508f52d60d0b8e4f930308d4dca86
chinmaypatil95 <chinmaypatil.jobs@gmail.com>
2022-07-27 11:50:44 +0530

    Added loop logic for chakra led pattern

---
+++ chakri_loop_led.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+#define A 5
+#define B 3
+int main()
+{
+    int x[A][B] = {0};
+    int count = 0;
+     for (int i = 0; i < A; i++)
+    {
+        for (int j = 0; j < B; j++)
+        {
+            count++;
+            if(i%2 == 0)
+            {
+                x[i][j] = count;
+            }
+            else
+            {
+                x[i][j] = x[i-1][B-j-1] + B;
+            }
+        }
+    }
+    // output each array element's value
+    for (int i = 0; i < A; i++)
+    {
+        for (int j = 0; j < B; j++)
+        {
+            cout << "x[" << i
+                 << "][" << j << "]: ";
+            cout << x[i][j]<<endl;
+        }
+    }
+
+
+    return 0;
+}
